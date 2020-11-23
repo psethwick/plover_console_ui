bcb5ca8249c3b98478d8d79ce4e83f4fade97023
Seth Rider <seth@thumbquat.com>
2020-11-23 21:48:27 +0000

    gui plugin installs, but does nothing

---
+++ plover_tui/main.py
@@ -0,0 +1,23 @@
+from threading import Event
+
+from plover.oslayer.keyboardcontrol import KeyboardEmulation
+
+from plover.gui_none.engine import Engine
+
+
+def show_error(title, message):
+    print('%s: %s' % (title, message))
+
+
+def main(config):
+    engine = Engine(config, KeyboardEmulation())
+    if not engine.load_config():
+        return 3
+    quitting = Event()
+    engine.hook_connect('quit', quitting.set)
+    engine.start()
+    try:
+        quitting.wait()
+    except KeyboardInterrupt:
+        engine.quit()
+    return engine.join()
