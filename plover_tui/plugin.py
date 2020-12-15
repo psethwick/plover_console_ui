d63fb75864c409f6f36c02d568a62fba2561e5f4
Seth Rider <seth@thumbquat.com>
2020-12-15 22:59:35 +0000

    Tidy up scene swapping from outside UI thread

--- plover_tui/plugin.py
+++ plover_tui/plugin.py
@@ -21,13 +21,6 @@ def show_error(title, message):
     print(title + message)
 
 
-def on_lookup(screen, engine):
-    focus_tui()
-    scenes = [
-        Scene([LookupView(screen, lookup_model, engine)], -1, name="Lookup"),
-        Scene([MainView(screen, main_model, engine)], -1, name="Main"),
-    ]
-    screen.set_scenes(scenes)
 
 
 def on_add_translation(screen, engine):
@@ -41,39 +34,45 @@ def on_add_translation(screen, engine):
 
 
 # I think I want a single column
-# TODO foxus!
 # TODO dictionary update
 # TODO I guess the config stuff
+# TODO turn off scroll bar?
 
 class MainModel():
-    def __init__(self, paper_tape, suggestions):
+    def __init__(self, paper_tape, suggestions, lookup_model):
         self.paper_tape = paper_tape
         self.suggestions = suggestions
+        self.lookup = lookup_model
 
 
 paper_tape_model = PaperTapeModel()
 lookup_model = LookupModel()
 suggestions_model = SuggestionsModel()
-main_model = MainModel(paper_tape_model, suggestions_model)
+main_model = MainModel(paper_tape_model, suggestions_model, lookup_model)
 last_scene = None
 
 
-def app(screen, scene, engine):
-    scenes = [
+def get_scenes(screen, main_model, engine, first_scene="Main"):
+    return sorted([
         Scene([MainView(screen, main_model, engine)], -1, name="Main"),
-        Scene([LookupView(screen, lookup_model, engine)], -1, name="Lookup"),
-    ]
+        Scene([LookupView(screen, main_model.lookup, engine)], -1, name="Lookup"),
+    ], key=lambda s: s.name != first_scene)
 
-    engine.hook_connect('stroked',
-                        partial(on_stroked, paper_tape_model))
-    engine.hook_connect('lookup',
-                        partial(on_lookup, screen, engine))
-    engine.hook_connect('translated',
-                        partial(on_translated, engine, suggestions_model))
-    engine.hook_connect('add_translation',
-                        partial(on_add_translation, screen, engine))
+
+def on_lookup(screen, engine):
+    focus_tui()
+    screen.set_scenes(get_scenes(screen, main_model, engine, "Lookup"))
+
+
+def app(screen, scene, engine):
+    engine.hook_connect('stroked', partial(on_stroked, paper_tape_model))
+    engine.hook_connect('lookup', partial(on_lookup, screen, engine))
+    engine.hook_connect(
+        'translated', partial(on_translated, engine, suggestions_model))
+    engine.hook_connect(
+        'add_translation', partial(on_add_translation, screen, engine))
     engine.start()
-    screen.play(scenes, start_scene=scene)
+    screen.play(get_scenes(screen, main_model, engine), start_scene=scene)
 
 
 def main(config):
